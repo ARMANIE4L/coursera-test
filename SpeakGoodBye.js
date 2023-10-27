fef038ec4d2389cc0445342191285a921edf35db
Izuchukwu Abugu <137216438+ARMANIE4L@users.noreply.github.com>
2023-10-27 05:07:44 +0100

    Delete SpeakGoodBye.js

--- SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-( (window) => {
-const byeSpeaker = {}
-const speakWord = 'Good Bye'
-byeSpeaker.speak = (name) => {
-  console.log(speakWord + " " + name);
-}
-  window.byeSpeaker = byeSpeaker
-})(window);
